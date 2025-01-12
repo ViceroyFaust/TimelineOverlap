e44a220cef042c36358f25a2d8de429f21c183c1
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2025-01-12 13:17:09 +0200

    Add constructor to Range

--- src/main/java/faust/viceroy/Range.java
+++ src/main/java/faust/viceroy/Range.java
@@ -4,6 +4,11 @@ public class Range {
   private int start;
   private int end;
 
+  public Range(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+
   public void setStart(int start) {
     this.start = start;
   }
